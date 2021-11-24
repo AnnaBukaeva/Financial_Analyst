843e7d421499e95e78073786a0831879c117de16
AnnaBukaeva <annabukaeva1@gmail.com>
2021-11-24 23:27:21 +0300

    Create project
    Создала проект. Сделала папки Data, Logic, View.

---
+++ Financial_Analyst/Financial_Analyst/Logic/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Financial_Analyst.Data;
+
+namespace Financial_Analyst.Logic
+{
+    class Class1
+    {
+    }
+}
---
+++ Financial_Analyst/Financial_Analyst/View/frmMain.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Financial_Analyst.View;
+using Financial_Analyst.Logic;
+
+namespace Financial_Analyst.View
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+    }
+}
