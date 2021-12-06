a1584ddcbba44f49830fdd3857a46f19a1cf5dea
Viktoriya_Bukaeva <vikvladkoval@gmail.com>
2021-12-06 23:08:08 +0300

    Исправление багов
    Исправлены баги в работе функции добавления счета, добавления транзакции.Исправлена проблема восстановления данных по транзакциям после запуска  программы.Исправлена проблема присвоения актуального ID

---
+++ Financial_Analyst/Financial_Analyst/Data/LastIDRepository.cs
@@ -0,0 +1,46 @@
+﻿using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+
+namespace Financial_Analyst.Data
+{
+    public class LastIDRepository
+    {
+        private static string lastID_filename = @"LastID.dat";
+
+        static LastIDRepository()
+        {
+            if (!File.Exists(lastID_filename))
+            {
+                using (FileStream fl = new FileStream(lastID_filename, FileMode.CreateNew))
+                {
+
+                }
+            }
+        }
+
+        public static int GetLastID()
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            int actual_lastID = 0;
+            using (FileStream fl = new FileStream(lastID_filename, FileMode.Open))
+            {
+                try
+                {
+                    actual_lastID = (int)bf.Deserialize(fl);
+                }
+                catch { }
+            }
+            return actual_lastID;
+        }
+        public static void SaveLastID(int lastID)
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            
+            using (FileStream fl = new FileStream(lastID_filename, FileMode.Create))
+            {
+                bf.Serialize(fl, lastID);
+            }
+        }
+    }
+}
--- Financial_Analyst/Financial_Analyst/Logic/Processors/CategoryProcessor.cs
+++ Financial_Analyst/Financial_Analyst/Logic/Processors/CategoryProcessor.cs
@@ -1,4 +1,5 @@
 ﻿using Financial_Analyst.Data;
+using System;
 using System.Collections.Generic;
 using System.Drawing;
 
@@ -15,6 +16,13 @@ namespace Financial_Analyst.Logic
 
         public static void CreateCategory (string name, string discription, Color categoryColor, CategoryType cType)
         {
+            foreach (ICategory item in _category)
+            {
+                if (item.Name == name)
+                {
+                    throw new ArgumentException("Категория с таким именем уже существует!");
+                }
+            }
             ICategory category = new Category (name, discription, categoryColor, cType);
             _category.Add(category);
             CategoryRepository.SaveCategory(category);
